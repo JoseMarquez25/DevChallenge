9ce218d61dc1c5ea817df9aaa77a28b806765a01
JoseMarquez25 <jmmarquezf@puce.edu.ec>
2025-07-04 16:42:59 -0500

    Actualización de settings.py

--- DevChProyect/settings.py
+++ DevChProyect/settings.py
@@ -121,3 +121,26 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIR = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / 'media'
+
+LOGIN_REDIRECT_URL='/index/'
+LOGOUT_REDIRECT_URL='/index/'
+LOGIN_URL = 'DevChApp:login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'marquezfjosem@gmail.com'
+EMAIL_HOST_PASSWORD = 'xarm tuzl srej emlj'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+AUTH_USER_MODEL = 'DevChApp.Usuario'
